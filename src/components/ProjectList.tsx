404112d7c3257301a03eff78840ea9b136f1fc8f
Randy Esplin <Randy.Esplin@gmail.com>
2025-02-09 14:04:41 -0800

    ProjectList - Add component

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,11 @@
+function ProjectList() {
+    return (
+        <>
+            <div>
+                <a href="/add" className={"btn btn-primary"}>Add Site</a>
+            </div>
+        </>
+    )
+}
+
+export default ProjectList
