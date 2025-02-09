8be8b9b3982972a6af2f03262bca0cffbb30ae14
Randy Esplin <Randy.Esplin@gmail.com>
2025-02-09 14:05:23 -0800

    Use ProjectList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header'
+import ProjectList from '../components/ProjectList'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '../../public/vite.svg'
 import '../styles/App.css'
@@ -11,9 +12,7 @@ function Home() {
             <main className={'container-fluid'}>
                 <div className={'row'}>
                     <div className={'col-12 col-md-5 col-xl-4'}>
-                        <div>
-                            <a href="/add" className={"btn btn-primary"}>Add Site</a>
-                        </div>
+                        <ProjectList />
                     </div>
 
                     <div className={'col-12 col-md-7 col-xl-8'}>
