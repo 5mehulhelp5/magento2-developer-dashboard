92ef7e9791b1a9134668cf7e2e4b5fcfb9b7b2cb
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-26 15:14:05 -0700

    Drop Baseball

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Baseball from './pages/Baseball';
 import Login from './pages/Login';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
         <>
             <Routes>
                 <Route index element={<Home />} />
-                <Route path="baseball" element={<Baseball />} />
                 <Route path="about" element={<About />} />
                 <Route path="login" element={<Login />} />
             </Routes>
