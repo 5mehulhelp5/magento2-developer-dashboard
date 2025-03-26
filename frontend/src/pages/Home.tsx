2d53d41dfefa7110bf862f5b3955ff70594f8606
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-26 16:40:09 -0700

    Home.tsx - Add div wrapper under main

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,21 +9,23 @@ function Home() {
         <>
             <Header />
 
-            <main className={'container-fluid'}>
-                <div className={'row'}>
-                    <div className={'col-12 col-md-5 col-xl-4'}>
-                        <ProjectList />
-                    </div>
+            <main>
+                <div className={'container-fluid'}>
+                    <div className={'row'}>
+                        <div className={'col-12 col-md-5 col-xl-4'}>
+                            <ProjectList />
+                        </div>
 
-                    <div className={'col-12 col-md-7 col-xl-8'}>
-                        <div>
-                            <h1>Vite + React</h1>
-                            <a href="https://vite.dev" target="_blank">
-                                <img src={viteLogo} className="logo" alt="Vite logo"/>
-                            </a>
-                            <a href="https://react.dev" target="_blank">
-                                <img src={reactLogo} className="logo react" alt="React logo"/>
-                            </a>
+                        <div className={'col-12 col-md-7 col-xl-8'}>
+                            <div>
+                                <h1>Vite + React</h1>
+                                <a href="https://vite.dev" target="_blank">
+                                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                                </a>
+                                <a href="https://react.dev" target="_blank">
+                                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
