fb89af05e1a310ffa59166e1b55d3ffd0809b328
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-26 15:13:50 -0700

    Drop Baseball

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -26,14 +26,6 @@ function Header() {
                             </li>
                             <li className="nav-item">
                                 <NavLink
-                                    to="/baseball"
-                                    className={({ isActive }) =>
-                                        isActive ? `nav-link active` : 'nav-link'
-                                    }
-                                >Baseball</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink
                                     to="/about"
                                     className={({ isActive }) =>
                                         isActive ? `nav-link active` : 'nav-link'
