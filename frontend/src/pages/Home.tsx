301e11a932ccab0e6bfe70df60a349066da7700a
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-29 13:58:51 -0700

    frontend - Drop vite and react images

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import Header from '../components/Header'
 import ProjectList from '../components/ProjectList'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '../../public/vite.svg'
 import '../styles/App.css'
 
 function Home() {
@@ -19,12 +17,6 @@ function Home() {
                         <div className={'col-12 col-md-7 col-xl-8'}>
                             <div>
                                 <h1>Vite + React</h1>
-                                <a href="https://vite.dev" target="_blank">
-                                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                                </a>
-                                <a href="https://react.dev" target="_blank">
-                                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                                </a>
                             </div>
                         </div>
                     </div>
