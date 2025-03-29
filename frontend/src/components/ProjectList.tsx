74cfd422a2e491d8c8c73284228a3f227fa43f75
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-29 13:59:41 -0700

    frontend ProjectList - Fix text for add button

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -2,7 +2,7 @@ function ProjectList() {
     return (
         <>
             <div>
-                <a href="/add" className={"btn btn-primary"}>Add Site</a>
+                <a href="/add" className={"btn btn-primary"}>Add Magento Site</a>
             </div>
         </>
     )
