ab6fc6912daf42c7c6650fc260297e34c9dd5e0f
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-29 14:00:46 -0700

    frontend Login - Add extra div wrapper

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,16 +8,18 @@ function Login() {
         <>
             <Header />
 
-            <main className={'container-fluid'}>
-                <div className={'row'}>
-                    <div className={'bg-info col-12 col-md-5 col-xl-4'}>
-                        <ProjectList />
-                    </div>
+            <main>
+                <div className={'container-fluid'}>
+                    <div className={'row'}>
+                        <div className={'col-12 col-md-5 col-xl-4'}>
+                            <ProjectList />
+                        </div>
 
-                    <div className={'col-12 col-md-7 col-xl-8'}>
-                        <div>
-                            <h1>Login</h1>
-                            <LoginForm />
+                        <div className={'col-12 col-md-7 col-xl-8'}>
+                            <div>
+                                <h1>Login</h1>
+                                <LoginForm />
+                            </div>
                         </div>
                     </div>
                 </div>
