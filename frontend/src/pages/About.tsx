5495ca281699ad846253f82aebbcb2170e19403f
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-29 14:01:33 -0700

    frontend About - Add extra div wrapper

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -2,20 +2,22 @@ import Header from '../components/Header'
 import ProjectList from '../components/ProjectList'
 import '../styles/App.css'
 
-function Home() {
+function About() {
     return (
         <>
             <Header />
 
-            <main className={'container-fluid'}>
-                <div className={'row'}>
-                    <div className={'bg-info col-12 col-md-5 col-xl-4'}>
-                        <ProjectList />
-                    </div>
+            <main>
+                <div className={'container-fluid'}>
+                    <div className={'row'}>
+                        <div className={'col-12 col-md-5 col-xl-4'}>
+                            <ProjectList />
+                        </div>
 
-                    <div className={'col-12 col-md-7 col-xl-8'}>
-                        <div>
-                            <h1>About Me</h1>
+                        <div className={'col-12 col-md-7 col-xl-8'}>
+                            <div>
+                                <h1>About Me</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -24,4 +26,4 @@ function Home() {
     )
 }
 
-export default Home
+export default About
