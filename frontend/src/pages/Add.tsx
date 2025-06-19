2208d16f7af63b1821a128cbd9e877efd088c8b7
Randy Esplin <randy.esplin@gmail.com>
2025-06-18 19:42:28 -0700

    frontend - Add Add.tsx

---
+++ frontend/src/pages/Add.tsx
@@ -0,0 +1,29 @@
+import Header from '../components/Header'
+import ProjectList from '../components/ProjectList'
+import '../styles/App.css'
+
+function Add() {
+    return (
+        <>
+            <Header />
+
+            <main>
+                <div className={'container-fluid'}>
+                    <div className={'row'}>
+                        <div className={'col-12 col-md-5 col-xl-4 leftPane'}>
+                            <ProjectList />
+                        </div>
+
+                        <div className={'col-12 col-md-7 col-xl-8 rightPane'}>
+                            <div>
+                                <h1>Add</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Add
