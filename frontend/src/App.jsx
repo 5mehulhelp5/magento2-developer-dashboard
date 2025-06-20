9e34e5c7d2498b0040a889573954771645b369ce
Randy Esplin <randy.esplin@gmail.com>
2025-06-19 20:01:49 -0700

    frontend/src/App.jsx - Add bootstrap css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import Add from './pages/Add';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
     return (
