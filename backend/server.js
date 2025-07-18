c52e5c557c7d1efc9fce53c9d11cd84e8bd67585
Randy Esplin <randy.esplin@ecinternet.com>
2025-07-18 15:53:18 -0700

    backend - Attempt to add /magento server route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import axios, { AxiosError } from "axios";
 import express from "express";
 import cors from "cors";
+import magentoRoutes from './routes/magentoRoutes.js'
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/magento', magentoRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from the backend!" });
