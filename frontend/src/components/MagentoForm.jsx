af8107abe77e8ab72f2b2508a31f37c40ecfd950
Randy Esplin <randy.esplin@ecinternet.com>
2025-07-25 09:36:48 -0700

    frontend - Add callback to refresh collection, use react HTML markup

--- frontend/src/components/MagentoForm.jsx
+++ frontend/src/components/MagentoForm.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default function MagentoForm() {
+export default function MagentoForm({ onRecordAdded }) {
     const submitForm = async(event) => {
         event.preventDefault();
 
@@ -11,7 +11,10 @@ export default function MagentoForm() {
         // Any validation
 
         try {
-            await axios.post('http://localhost:5000/magento/', magentoData)
+            await axios.post('http://localhost:5000/magento/', magentoData);
+
+            // Call to parent component to refresh records
+            onRecordAdded();
         } catch (error) {
             console.error('Error submitting form:', error);
         }
@@ -21,16 +24,16 @@ export default function MagentoForm() {
         <div className="form-wrapper">
             <div className="flex h-screen bg-gray-100">
                 <form onSubmit={submitForm}>
-                    <div class="form-group">
-                        <label for="name" className="form-label">Site Name</label>
+                    <div className="form-group">
+                        <label htmlFor="name" className="form-label">Site Name</label>
                         <input name="name" type="text" placeholder="Site Name" className="form-control" required /><br />
                     </div>
 
-                    <label for="url" className="form-label">Magento URL</label>
+                    <label htmlFor="url" className="form-label">Magento URL</label>
                     <input name="url" type="text" placeholder="Magento URL" className="form-control" required /><br />
 
-                    <label for="access_token" className="form-label">Magento Token</label>
-                    <input name="access_token" type="text" placeholder="Magento Token" /><br />
+                    <label htmlFor="access_token" className="form-label">Magento Token</label>
+                    <input name="access_token" type="text" placeholder="Magento Token" className="form-control" /><br />
 
                     <button type="submit" className="btn btn-primary">Add Magento</button>
                 </form>
--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,10 +1,10 @@
 import MagentoForm from '../components/MagentoForm'
 
-export default function Add() {
+export default function Add({ onRecordAdded}) {
     return (
         <>
             <h1>Add</h1>
-            <MagentoForm />
+            <MagentoForm onRecordAdded={onRecordAdded} />
         </>
     )
 }
