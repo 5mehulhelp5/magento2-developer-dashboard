59ef264f6163aceab7c3f0f9e9711d096ef95d07
Randy Esplin <randy.esplin@gmail.com>
2025-07-26 14:33:35 -0700

    frontend - More text align, add url to ProjectList

--- frontend/src/components/ProjectList.jsx
+++ frontend/src/components/ProjectList.jsx
@@ -3,12 +3,21 @@ import { Link } from 'react-router-dom';
 export default function ProjectList({ records }) {
     return (
         <>
-            <Link to="/add" className="btn btn-primary">Add Magento Site</Link>
+            <p className="text-center">
+                <Link to="/add" className="btn btn-primary">Add Magento Site</Link>
+            </p>
 
             {records && records.length > 0 ? (
                 <ul className="list-group">
                     {records.length &&
-                        records.map(r => <Link to={`/magento/${r.id}`} className="list-group-item" key={r.id}>{r.name}</Link>)
+                        records.map(
+                            r => <Link
+                                to={`/magento/${r.id}`}
+                                className="list-group-item"
+                                key={r.id}>
+                                {r.name} - {r.url}
+                            </Link>
+                        )
                     }
                 </ul>
             ) : (
