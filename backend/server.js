ffb4afefe884f46b665086563a630753c53cd65f
Randy Esplin <randy.esplin@gmail.com>
2025-09-07 17:56:20 -0700

    backend/server.js - Remove old testing

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import axios, { AxiosError } from "axios";
 import express from "express";
 import cors from "cors";
 import magentoRoutes from './routes/magentoRoutes.js'
@@ -17,47 +16,6 @@ app.use('/magento', magentoRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from the backend!" });
-});
-
-app.get('/api/customers', async (req, res) => {
-    try {
-        const response = await axios.get('http://magento2.docker/rest/V1/customers/search?searchCriteria[filter_groups][0][filters][0][field]=updated_at&searchCriteria[filter_groups][0][filters][0][value]=2022-12-25 20:21:01&searchCriteria[filter_groups][0][filters][0][condition_type]=gt', {
-            headers: {
-                'Authorization': `Bearer 55e6xxg0k0r6mlqfypo420e93m5nw8oz`
-            }
-        })
-        res.json(response.data);
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            console.log(error.message)
-            res.json({ errorMessage: error.message });
-        } else {
-            console.log(error)
-            res.json({errorMessage: 'Unknown error fetching customers!'});
-        }
-    }
-});
-
-app.post('/customer/token', async (req, res) => {
-    try {
-        const response = await axios.post('http://magento2.docker/rest/V1/integration/customer/token', {
-            username: req.body.email,
-            password: req.body.password
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        res.json(response.data);
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            console.log(error.message)
-            res.json({ errorMessage: error.message });
-        } else {
-            console.error(error);
-            res.json({errorMessage: 'Unknown error fetching customer token.'});
-        }
-    }
 });
 
 const connectWithRetry = async (retries = 5, delay = 3000) => {
