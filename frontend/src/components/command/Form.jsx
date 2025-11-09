b15c1f1fda398b87d01c6e107104abc16f4dfbb0
Randy Esplin <randy.esplin@gmail.com>
2025-11-09 14:53:54 -0800

    frontend - Drop 'public_html' checkbox

--- frontend/src/components/command/Form.jsx
+++ frontend/src/components/command/Form.jsx
@@ -34,11 +34,6 @@ export default function MagentoForm({ onSuccess }) {
                     <label htmlFor="value" className="form-label">Command</label>
                     <input id="value" name="value" type="text" placeholder="php bin/magento..." className="form-control" required /><br />
 
-                    <div className="form-check">
-                        <label htmlFor="change_folder" className="form-check-label">Run from public_html/ ?</label>
-                        <input id="change_folder" name="change_folder" type="checkbox" className="form-check-input"/><br/>
-                    </div>
-
                     <p className="text-center">
                         <button type="submit" className="btn btn-primary">Add Command</button>
                     </p>
