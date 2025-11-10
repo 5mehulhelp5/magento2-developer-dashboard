9ad2c3fc73c621a59b301bc57b4b6e069847e934
Randy Esplin <randy.esplin@gmail.com>
2025-11-09 17:14:29 -0800

    backend - add getCommand to routes

--- backend/src/routes/commandRoutes.js
+++ backend/src/routes/commandRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     createCommand,
     getCommands,
+    getCommand,
     updateCommand,
     deleteCommand
 } from '../controllers/commandController.js';
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get('/', getCommands);
 router.post('/', createCommand);
+router.get('/:id', getCommand)
 router.put('/:id', updateCommand);
 router.delete('/:id', deleteCommand);
 
